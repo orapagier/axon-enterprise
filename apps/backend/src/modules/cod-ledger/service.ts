ff71aa2074a2b28b6713f41fde5a6202266d1378
Jelmar Orapa <orapajelmar@gmail.com>
2026-05-28 22:41:56 +0800

    claude: auto-backup 22:41:56

--- apps/backend/src/modules/cod-ledger/service.ts
+++ apps/backend/src/modules/cod-ledger/service.ts
@@ -1,9 +1,7 @@
 import { MedusaService } from "@medusajs/framework/utils"
-import BuyerWallet from "./models/buyer-wallet"
 import CodTransaction from "./models/cod-transaction"
 
 class CodLedgerModuleService extends MedusaService({
-  BuyerWallet,
   CodTransaction,
 }) {}
 
