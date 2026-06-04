9b99b3c947746a9e643084f134449fb65aac3106
Jelmar Orapa <orapajelmar@gmail.com>
2026-06-05 04:04:58 +0800

    claude: auto-backup 04:04:58

--- apps/storefront/src/modules/account/components/address-book/index.tsx
+++ apps/storefront/src/modules/account/components/address-book/index.tsx
@@ -13,13 +13,13 @@ const AddressBook: React.FC<AddressBookProps> = ({ customer, region }) => {
   const { addresses } = customer
   return (
     <div className="w-full">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 flex-1 mt-4">
-        <AddAddress region={region} addresses={addresses} />
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
         {addresses.map((address) => {
           return (
             <EditAddress region={region} address={address} key={address.id} />
           )
         })}
+        <AddAddress region={region} addresses={addresses} />
       </div>
     </div>
   )
