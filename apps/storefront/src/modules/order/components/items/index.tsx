83f62df9a301e1b9a66dba0581317cb4007dda04
Jelmar Orapa <orapajelmar@gmail.com>
2026-06-04 19:37:02 +0800

    claude: auto-backup 19:37:02

--- apps/storefront/src/modules/order/components/items/index.tsx
+++ apps/storefront/src/modules/order/components/items/index.tsx
@@ -1,10 +1,7 @@
 import repeat from "@lib/util/repeat"
 import { HttpTypes } from "@medusajs/types"
-import { Table } from "@modules/common/components/ui"
 
-import Divider from "@modules/common/components/divider"
 import Item from "@modules/order/components/item"
-import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
 
 type ItemsProps = {
   order: HttpTypes.StoreOrder
@@ -14,29 +11,32 @@ const Items = ({ order }: ItemsProps) => {
   const items = order.items
 
   return (
-    <div className="flex flex-col">
-      <Divider className="!mb-0" />
-      <Table>
-        <Table.Body data-testid="products-table">
-          {items?.length
-            ? items
-                .sort((a, b) => {
-                  return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
-                })
-                .map((item) => {
-                  return (
-                    <Item
-                      key={item.id}
-                      item={item}
-                      currencyCode={order.currency_code}
-                    />
-                  )
-                })
-            : repeat(5).map((i) => {
-                return <SkeletonLineItem key={i} />
-              })}
-        </Table.Body>
-      </Table>
+    <div
+      className="flex flex-col divide-y divide-grey-10"
+      data-testid="products-table"
+    >
+      {items?.length
+        ? [...items]
+            .sort((a, b) =>
+              (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
+            )
+            .map((item) => (
+              <Item
+                key={item.id}
+                item={item}
+                currencyCode={order.currency_code}
+              />
+            ))
+        : repeat(3).map((i) => (
+            <div key={i} className="flex items-center gap-4 py-4">
+              <div className="h-16 w-16 shrink-0 animate-pulse rounded-large bg-grey-10" />
+              <div className="flex flex-1 flex-col gap-y-2">
+                <div className="h-4 w-32 animate-pulse rounded bg-grey-10" />
+                <div className="h-3 w-20 animate-pulse rounded bg-grey-10" />
+              </div>
+              <div className="h-4 w-16 animate-pulse rounded bg-grey-10" />
+            </div>
+          ))}
     </div>
   )
 }
