3d6e1feea2d8d143eb5d2d5304163578c41ba844
Jelmar Orapa <orapajelmar@gmail.com>
2026-06-13 15:41:25 +0800

    claude: auto-backup 15:41:25

--- apps/storefront/src/modules/account/components/overview/index.tsx
+++ apps/storefront/src/modules/account/components/overview/index.tsx
@@ -1,5 +1,6 @@
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import AccountStatusBanner from "@modules/account/components/account-status-banner"
+import PushOptIn from "@modules/account/components/push-opt-in"
 import { convertToLocale } from "@lib/util/money"
 import { hasRole } from "@lib/util/roles"
 import { HttpTypes } from "@medusajs/types"
