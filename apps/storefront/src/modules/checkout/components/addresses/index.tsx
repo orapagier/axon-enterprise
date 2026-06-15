5ab6653605e68c78c04e7eb388ff5baf28dd6a10
Jelmar Orapa <orapajelmar@gmail.com>
2026-06-15 22:12:23 +0800

    claude: auto-backup 22:12:23

--- apps/storefront/src/modules/checkout/components/addresses/index.tsx
+++ apps/storefront/src/modules/checkout/components/addresses/index.tsx
@@ -73,6 +73,7 @@ const Addresses = ({
               checked={sameAsBilling}
               onChange={toggleSameAsBilling}
               cart={cart}
+              onValidityChange={setShippingValid}
             />
 
             {!sameAsBilling && (
